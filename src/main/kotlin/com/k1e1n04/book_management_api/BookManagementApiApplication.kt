c61b802291efcc9bfcdea100a1b0de521e255d6f
KenIshii <riguveda44@gmail.com>
2025-06-08 17:38:14 +0900

    first commit

---
+++ src/main/kotlin/com/k1e1n04/book_management_api/BookManagementApiApplication.kt
@@ -0,0 +1,10 @@
+package com.k1e1n04.book_management_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookManagementApiApplication
+fun main(args: Array<String>) {
+	runApplication<BookManagementApiApplication>(*args)
+}
