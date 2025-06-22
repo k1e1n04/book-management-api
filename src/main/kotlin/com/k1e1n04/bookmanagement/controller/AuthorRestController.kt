3f32f59214be016e3412a094f087ae5b1d40ba0c
KenIshii <riguveda44@gmail.com>
2025-06-22 18:08:04 +0900

    fix: registerAuthor Status

--- src/main/kotlin/com/k1e1n04/bookmanagement/controller/AuthorRestController.kt
+++ src/main/kotlin/com/k1e1n04/bookmanagement/controller/AuthorRestController.kt
@@ -4,12 +4,14 @@ import com.k1e1n04.bookmanagement.request.AuthorRegisterRequest
 import com.k1e1n04.bookmanagement.request.AuthorUpdateRequest
 import com.k1e1n04.bookmanagement.service.AuthorService
 import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 /**
@@ -35,10 +37,10 @@ class AuthorRestController(
      * @return 登録された著者の情報
      */
      @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun registerAuthor(
         @RequestBody @Valid author: AuthorRegisterRequest
-    ) =
-        authorService.registerAuthor(author)
+    ) = authorService.registerAuthor(author)
 
     /**
      * 著者情報を更新
