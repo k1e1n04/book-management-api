e2c3fe793140303fd946f76808b2f23ac337d004
KenIshii <riguveda44@gmail.com>
2025-06-22 17:03:26 +0900

    add: RepositoryTest Annotation

---
+++ src/test/kotlin/com/k1e1n04/bookmanagement/repository/RepositoryTest.kt
@@ -0,0 +1,20 @@
+package com.k1e1n04.bookmanagement.repository
+
+import com.k1e1n04.bookmanagement.config.TestcontainersConfiguration
+import org.springframework.boot.test.autoconfigure.jooq.JooqTest
+import org.springframework.context.annotation.Import
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.TestPropertySource
+import org.springframework.transaction.annotation.Transactional
+
+/**
+ * リポジトリテスト用のアノテーション
+ *
+ *  - DSLContextなどのJOOQ関連のBeanが自動で設定される
+ *  - Testcontainersを使用してPostgreSQLのテスト環境が構築される
+ *  - トランザクション管理が有効になり、テスト後にロールバックされる
+ */
+@JooqTest
+@Import(TestcontainersConfiguration::class)
+@Transactional
+annotation class RepositoryTest
