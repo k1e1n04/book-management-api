92dad9a4387741e57f305e20d7fa4b0d25a97dab
KenIshii <riguveda44@gmail.com>
2025-06-22 17:05:32 +0900

    add: TestcontainersConfiguration

---
+++ src/test/kotlin/com/k1e1n04/bookmanagement/config/TestcontainersConfiguration.kt
@@ -0,0 +1,20 @@
+package com.k1e1n04.bookmanagement.config
+
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection
+import org.springframework.context.annotation.Bean
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.utility.DockerImageName
+
+/**
+ * Testcontainersを使用したPostgreSQLの設定
+ */
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+    @Bean
+    @ServiceConnection
+    fun postgresContainer(): PostgreSQLContainer<*> {
+        return PostgreSQLContainer(DockerImageName.parse("postgres:15-alpine"))
+    }
+}
