c815ed5d33ca2c9b85d82802c6928c490d1a373d
KenIshii <riguveda44@gmail.com>
2025-06-25 00:15:25 +0900

    refactor: import TestcontainersConfiguration for enhanced test environment setup

--- src/test/kotlin/com/k1e1n04/bookmanagement/BookManagementApplicationTests.kt
+++ src/test/kotlin/com/k1e1n04/bookmanagement/BookManagementApplicationTests.kt
@@ -1,9 +1,12 @@
 package com.k1e1n04.bookmanagement
 
+import com.k1e1n04.bookmanagement.config.TestcontainersConfiguration
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
 
 @SpringBootTest
+@Import(TestcontainersConfiguration::class)
 class BookManagementApplicationTests {
     @Test
     fun contextLoads() {
